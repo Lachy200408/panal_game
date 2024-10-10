64c5a2bcbb2337f30c3923c3a136013ff42c7bd3
Jean1408Man <153570043+Jean1408Man@users.noreply.github.com>
2024-10-09 22:14:59 -0400

    Adding an envirom=nment for more Sounds

--- Assets/Scripts/Music/Sound Manager.cs
+++ Assets/Scripts/Music/Sound Manager.cs
@@ -6,25 +6,47 @@ using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
+    //Estas serian las fuentes de Audio
     [SerializeField]public AudioSource Music;
     [SerializeField]public AudioSource Effects;
 
-    [SerializeField] public Slider MusicSlider;
-    [SerializeField] public Slider EffectsSlider;
+    //Estos son los ScrollBar, para controlar el volumen de ambas fuentes
+    [SerializeField] public Scrollbar MusicSlider;
+    [SerializeField] public Scrollbar EffectsSlider;
 
+    //Aqui irian las musicas de fondo
     public AudioClip BackGround;
+    //Aqui irian los clips
     public AudioClip ClickSound;
 
     void Start()
     {
         Music.clip = BackGround;
+
+        PlayMusic("background");
+    }
+    public void PlayFX(string sound)
+    {//Este metodo se encarga de producir en la fuente FX( Efectos) el sonido que se ordene
+        if(sound == "click")
+            Effects.PlayOneShot(ClickSound);
+        //...
+    }
+    public void PlayMusic(string sound)
+    {
+        if(sound == "background")
+            Music.clip = BackGround;
         Music.Play();
     }
-    public void PlayClick()
+
+    public void ChangingVolume(string source)
     {
-        Effects.PlayOneShot(ClickSound);
+        if(source == "Music")
+            Music.volume= MusicSlider.value;
+        else if(source == "Effects")
+            Effects.volume= EffectsSlider.value;
     }
-    
+
+
 
     
 }
